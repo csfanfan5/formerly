b3416d23a200e5f9aabdf9a8aff3c9a0d77f1e22
COLLIN FAN <cfan@college.harvard.edu>
2025-11-17 15:37:32 -0500

    Fix

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ Vercel looks for a module (e.g., api/app.py) that exports a Flask
 application named `app`. We re-export the `app` defined in page_answers.
 """
 
-from page_answers import app  # noqa: F401
+from api.page_answers import app  # noqa: F401
 
 
